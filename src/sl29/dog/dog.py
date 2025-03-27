ae922a5d3a8e6f2044d39b11d05f8c87d1f86c38
titouSIO <lefalchier-t@saint-louis29.net>
2025-03-27 16:14:55 +0100

    version_fin_chien

--- src/sl29/dog/dog.py
+++ src/sl29/dog/dog.py
@@ -30,6 +30,10 @@ class Dog:
         self._race = race  # Attribut protégé pour la race
         self._sex = sex    # Attribut protégé pour le sexe
         self.name = name   # Attribut public pour le nom
+        self._mother = None
+        self._father = None
+        self._puppies = []
+
 
     @property
     def race(self) -> str:
@@ -53,8 +57,9 @@ class Dog:
         DOCSTRINGS A COMPLETER
         """
         return f"Chien: {self.name}, Race: {self._race}, Sexe: {self._sex}"
-    
 
+    
+    
     
     def bark(self, n=1):
         return "Woff" * n
@@ -62,3 +67,80 @@ class Dog:
     def chew(self, stuff):
         a_stuff = stuff[:-1]
         return a_stuff
+
+   
+
+    @property
+    def mother(self) -> Optional['Dog']:
+        """
+        Retourne la mère du chien ou None.
+        
+        Returns:
+            Optional[Dog]: La mère du chien ou None.
+        """
+        return self._mother
+
+    @property
+    def father(self) -> Optional['Dog']:
+        """
+        Retourne la mère du chien ou None.
+        
+        Returns:
+            Optional[Dog]: La mère du chien ou None.
+        """
+        return self._father
+
+
+    @property
+    def puppies(self) -> Optional['Dog']:
+        """
+        Retourne la mère du chien ou None.
+        
+        Returns:
+            Optional[Dog]: La mère du chien ou None.
+        """
+        return self._puppies
+
+
+    def mate(self, other: 'Dog') -> 'Dog':
+        """
+        Fait s'accoupler deux chiens et retourne un chiot.
+
+        Args:
+            other (Dog): L'autre chien avec lequel s'accoupler.
+
+        Returns:
+            Dog: Le chiot issu de l'accouplement.
+
+        Raises:
+            MatingError: Si les deux chiens sont de même sexe.
+        """
+        # Vérification des sexes
+        # Détermination du père et de la mère
+        # Détermination de la race du chiot
+        # Détermination du sexe du chiot (aléatoire)
+        # Création du chiot
+        # Assignation des parents
+        # Ajout du chiot à la liste des chiots des parents
+        if self._sex == other._sex:
+            raise MatingError("Ils sont du même sexe, donc l'accouplement est impossible.")
+        else:
+            father = self if self._sex == "M" else other
+            mother = self if self._sex == "F" else other
+
+            if father.race == mother.race:
+                race = father.race
+            else:
+                race= "bâtard"
+
+            puppies_sex = random.choice(["M", "F"])
+
+            puppies = Dog( race=race,sex=puppies_sex, name="chiotA")
+
+            puppies._father = father.name
+            puppies._mother = mother.name
+
+            father._puppies.append(puppies)
+            mother._puppies.append(puppies)
+
+        return puppies
